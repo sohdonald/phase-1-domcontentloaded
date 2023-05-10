9ac3551339747731e2dcc645c4cd106e6e01b70a
Donald Soh <sohdonald@gmail.com>
2023-05-10 16:19:01 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("text").textContent = 
+    "This is really cool!";    
+  });
+
+
+//   document.addEventListener("DOMContentLoaded", () => {
+//     console.log("after Dom loaded");
+//   })
+
+//     console.log("before dom loads"); 
